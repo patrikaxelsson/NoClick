6afc2a1cd95f297098de773ab34bd7438cc2b42c
Patrik Axelsson <patrik.bo@gmail.com>
2019-03-16 18:23:41 +0100

    Reorganize to be more correct and do a CreateIORequest() for each OpenDevice().

--- NoClick.c
+++ NoClick.c
@@ -31,22 +31,20 @@ __saveds LONG NoClick(void) {
 	}
 	
 	struct MsgPort *trackDiskPort = CreateMsgPort();
-	if(NULL != trackDiskPort) {
+	for(ULONG i = 0; i < NUMUNITS; i++) {
 		struct IOExtTD *trackDiskReq = (struct IOExtTD *) CreateIORequest(trackDiskPort, sizeof(struct IOExtTD));
 		if(NULL != trackDiskReq) {
-			for(ULONG i = 0; i < NUMUNITS; i++) {
-				if(0 == OpenDevice(TD_NAME, i, (struct IORequest *) trackDiskReq, 0)) {
-					struct TDU_PublicUnit *publicUnit = (struct TDU_PublicUnit *) trackDiskReq->iotd_Req.io_Unit;
-					Forbid();
-					publicUnit->tdu_PubFlags |= TDPF_NOCLICK;
-					Permit();
-					CloseDevice((struct IORequest *) trackDiskReq);
-				}
+			if(0 == OpenDevice(TD_NAME, i, (struct IORequest *) trackDiskReq, 0)) {
+				struct TDU_PublicUnit *publicUnit = (struct TDU_PublicUnit *) trackDiskReq->iotd_Req.io_Unit;
+				Forbid();
+				publicUnit->tdu_PubFlags |= TDPF_NOCLICK;
+				Permit();
+				CloseDevice((struct IORequest *) trackDiskReq);
 			}
 			DeleteIORequest((struct IORequest *) trackDiskReq);
 		}
-		DeleteMsgPort(trackDiskPort);
 	}
+	DeleteMsgPort(trackDiskPort);
 
 	if(NULL != wbStartupMsg) {
 		Forbid();
