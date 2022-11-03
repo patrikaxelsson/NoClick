67b87be46df983d0e987d1be054a9aed53d558c8
Patrik Axelsson <patrik.bo@gmail.com>
2022-11-03 22:43:19 +0100

    Use RTF_AFTERDOS to guarantee success

--- Resident.c
+++ Resident.c
@@ -7,17 +7,25 @@ const struct Resident ROMTag = {
 	.rt_MatchWord = RTC_MATCHWORD,
 	.rt_MatchTag  = (void *) &ROMTag,
 	.rt_EndSkip   = (void *) &EndVariable,
+	// RTF_AFTERDOS is used to make sure we run NoClick() after all units
+	// have been mounted. If we run at RTF_COLDSTART, then depending on system
+	// configuration, it is possible that there is a long enough time between
+	// our unit close and the next open by the fs mounting, so trackdisk will
+	// notice that it has no users and clean up the unit data including the
+	// TDPF_NOCLICK flag bit NoClick() just set.
+	// So RTF_AFTERDOS guarantees that NoClick() works, at the cost of an
+	// extra click or two at boot.
 	#if defined(__amigaos4__)
-	.rt_Flags     = RTF_COLDSTART | RTF_NATIVE,
+	.rt_Flags     = RTF_AFTERDOS | RTF_NATIVE,
 	#elif defined(__MORPHOS__)
-	.rt_Flags     = RTF_COLDSTART | RTF_PPC,
+	.rt_Flags     = RTF_AFTERDOS | RTF_PPC,
 	#else
-	.rt_Flags     = RTF_COLDSTART,
+	.rt_Flags     = RTF_AFTERDOS,
 	#endif
 	.rt_Version   = 2,
 	.rt_Type      = NT_UNKNOWN, // We just want to run rt_Init
-	.rt_Pri       = 0, // Need to be initialized later than trackdisk at 20
+	.rt_Pri       = 0,
 	.rt_Name      = "NoClick",
-	.rt_IdString  = "NoClick 2.0 (31.10.2022) by Patrik Axelsson",
+	.rt_IdString  = "NoClick 2.0 (3.11.2022) by Patrik Axelsson",
 	.rt_Init      = (void *) &ResidentInit
 };
